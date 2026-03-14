c8d732b0399a8885d3523f1009766da2a6b12d97
Iamanthonycruz <Charliecruz1224@gmail.com>
2026-03-14 00:53:48 -0700

    Feat(memory) Add save note handler and get notes handler to the research agent

--- tests/test.py
+++ tests/test.py
@@ -1,26 +1,20 @@
-import asyncio
-from http.client import HTTPException
 
-from pydantic import HttpUrl
-import trafilatura
-
-
-async def page_fetcher_handler(url: str):
-    try:
-        fetched_page = trafilatura.fetch_url(url)
-        if not fetched_page:
-            return f"Failed to fetch {url}"
-        result = trafilatura.extract(fetched_page)
-        if not result:
-            return f"Fetched url but failed to extract content from {url}"
-        result_arr = result.split()
-        result_arr = result_arr[:3000]
-        return " ".join(result_arr)
-    except Exception as e:
-        return f"Error fetching {url}: {str(e)}"
-
-async def main():
-    res = await page_fetcher_handler("https://en.wikipedia.org/wiki/Fallout_(franchise)")
-    #print(res)
+notes = []
+def save_note_handler(key: str, content:str, notes:list):
+    if not key or not content or not notes:
+        return "Key or content are missing"
+    my_dict = {
+        "key": key,
+        "content":content
+    }
+    notes.append(my_dict)
     
-asyncio.run(main())
+def get_notes_handler(notes:list):
+    note_arr = []
+    if not notes:
+        return "No stored notes found"
+    for index, note in enumerate(notes,start=1):
+        print(note)
+        note_str = f"Note: [{index}] Key:{note["content"]} Content: {note["content"]}"
+        note_arr.append(note_str)
+    return "\n ".join(note_arr)
